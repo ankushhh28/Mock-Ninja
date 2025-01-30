7cc3294ae34d32e433a80a67ae7190b31e9ec03d
Anish_Saini <anishsaini9098@gmail.com>
2025-01-31 03:40:46 +0530

    Update DataProvider.jsx

--- Client/src/Context/DataProvider.jsx
+++ Client/src/Context/DataProvider.jsx
@@ -4,7 +4,7 @@ export const DataContext = createContext(null);
 
 const DataProvider = ({ children }) => {
 
-  const backendUrl = "http://localhost:5000";
+  const backendUrl = "https://mock-ninja-backend.onrender.com";
 
   return (
 
