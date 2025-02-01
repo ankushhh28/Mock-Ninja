32c3cdaf7b220e2484007a6db66c0dd9df960954
Ishita127 <rathiishu017@gmail.com>
2025-02-01 23:29:49 +0530

    changes to frontend(home-candidate)

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import Layout from "../Layout/Layout";
 import InfiniteSlider from "../Components/InfiniteSlider";
 import ImageSlider from "../Components/ImageSlider";
+import MockDescSection from "../Components/MockDescSection";
+import StepsHome from "../Components/StepsHome";
 
 const Home = () => {
   return (
     <>
     <Layout>
-      
+      <div>
       <ImageSlider/>
-
       <InfiniteSlider/>
-
+      <MockDescSection />
+      <StepsHome />
+    </div>
     </Layout>
     </>
   )
